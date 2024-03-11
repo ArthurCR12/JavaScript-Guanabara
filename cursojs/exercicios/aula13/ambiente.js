9bec11aa549c695444b9154f2ac0d1e7e515abe6
Arthur Cabral <134966248+ArthurCR12@users.noreply.github.com>
2024-03-11 09:39:36 -0300

    Inicio ex aula 14

---
+++ cursojs/exercicios/aula13/ambiente.js
@@ -0,0 +1,11 @@
+var contador = 1
+while (contador < 6){
+    console.log(`While ${contador}°`)    
+    contador++
+}
+
+
+do{
+    console.log(`Do While ${contador}°`)
+    contador++
+} while (contador < 11)
